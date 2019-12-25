7f40c340b45b54280f853e7d613b40889296b876
Stoyan Stoyanov <stoyan@lionheartapplications.com>
2019-12-25 11:50:37 +0200

    removed old file systems, left only apfs for less confusion

--- Sources/DiskUtil/FileSystem.swift
+++ Sources/DiskUtil/FileSystem.swift
@@ -18,16 +18,16 @@ public enum FileSystem: String, CaseIterable {
     
     case caseSensitiveAPFS = "Case-sensitive APFS"
     case apfs = "APFS"
-    case exFAT = "ExFAT"
+//    case exFAT = "ExFAT"
 //    case freeSpace = "Free Space"
 //    case msDOS = "MS-DOS"
 //    case fat12 = "MS-DOS FAT12"
 //    case fat16 = "MS-DOS FAT16"
 //    case fat32 = "MS-DOS FAT32"
-    case hfsPlus = "HFS+"
-    case caseSensitiveHfsPlus = "Case-sensitive HFS+"
-    case caseSensitiveJournaledHfsPlus = "Case-sensitive Journaled HFS+"
-    case journaledHfsPlus = "Journaled HFS+"
+//    case hfsPlus = "HFS+"
+//    case caseSensitiveHfsPlus = "Case-sensitive HFS+"
+//    case caseSensitiveJournaledHfsPlus = "Case-sensitive Journaled HFS+"
+//    case journaledHfsPlus = "Journaled HFS+"
 }
 
 // MARK: - User Facing Name
@@ -39,16 +39,16 @@ extension FileSystem {
         switch self {
         case .caseSensitiveAPFS: return "APFS (Case-sensitive)"
         case .apfs: return "APFS"
-        case .exFAT: return "ExFAT"
+//        case .exFAT: return "ExFAT"
 //        case .freeSpace: return "Free Space"
 //        case .msDOS: return "MS-DOS (FAT)"
 //        case .fat12: return "MS-DOS (FAT12)"
 //        case .fat16: return "MS-DOS (FAT16)"
 //        case .fat32: return "MS-DOS (FAT32)"
-        case .hfsPlus: return "Mac OS Extended"
-        case .caseSensitiveHfsPlus: return "Mac OS Extended (Case-sensitive)"
-        case .caseSensitiveJournaledHfsPlus: return "Mac OS Extended (Case-sensitive, Journaled)"
-        case .journaledHfsPlus: return "Mac OS Extended (Journaled)"
+//        case .hfsPlus: return "Mac OS Extended"
+//        case .caseSensitiveHfsPlus: return "Mac OS Extended (Case-sensitive)"
+//        case .caseSensitiveJournaledHfsPlus: return "Mac OS Extended (Case-sensitive, Journaled)"
+//        case .journaledHfsPlus: return "Mac OS Extended (Journaled)"
         }
     }
 }
